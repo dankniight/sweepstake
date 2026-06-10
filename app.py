30345342b8ce2f4426e2618ef907c4f64c7b5851
Dan Knight <82117525+dankniight@users.noreply.github.com>
2026-06-10 11:53:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def reset_players():
     return jsonify(players)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
